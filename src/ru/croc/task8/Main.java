c1c063f4bbcd86078feb04514f1df02cfe1624c1
rioleo2 <rioleo2@mail.ru>
2022-11-09 11:55:31 +0300

    -исправил 8 задачу

--- src/ru/croc/task8/Main.java
+++ src/ru/croc/task8/Main.java
@@ -12,7 +12,8 @@ public class Main {
 
         try (Scanner keyboard = new Scanner(new FileReader(args[0]))) {
             while (keyboard.hasNextLine()) {
-                numberOfWords += keyboard.nextLine().split("\s+").length; //к счетчику numberOfWords прибавляется
+                if (!keyboard.next().equals("\n"))
+                    numberOfWords += keyboard.nextLine().split("\s+").length; //к счетчику numberOfWords прибавляется
                                                                                 //длина строки, разделенная одним
                                                                                 //или несколькими пробелами
                                                                                 //(\s+ - метасимвол пробела или
